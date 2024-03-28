0222aec836f876a51e948c1295a91bdddfc25995
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-03-29 01:36:33 +0900

    Go 문법

--- GO/변수선언.go
+++ GO/변수선언.go
@@ -4,24 +4,30 @@ import "fmt"
 
 func 변수() {
 	// 변수선언 var
-	var num1 int = 10;
+	var num1 int = 10
 	fmt.Printf("num1: %d\n", num1) // num1: 10 (int로 선언)
 
 	// 변수선언 타입추론
-	var num2 = 20;
+	var num2 = 20
 	fmt.Printf("num2: %d\n", num2) // num2: 20 (자동으로 추론해서 int로 선언)
 
 	// 변수선언 축약형
-	num3 := 30;
+	num3 := 30
 	fmt.Printf("num3: %d\n", num3) // num3: 30 (자동으로 추론해서 int로 선언) -> 왈루스
 
 	// 변수선언 다중
-	var num4, num5 int = 40, 50;
+	var num4, num5 int = 40, 50
 	fmt.Printf("num4: %d, num5: %d\n", num4, num5) // num4: 40, num5: 50 (int로 선언)
 
 	// 상수선언 const
-	const num6 int = 40;
+	const num6 int = 40
 	fmt.Printf("num6: %d\n", num6) // num4: 40 (int로 선언)
 	// num6 = 10; 상수는 변경 불가능 -> 에러
 
+	// 상수선언 다중
+	const (
+		num7 int = 50
+		num8 int = 60
+	)
+	fmt.Printf("num7: %d, num8: %d\n", num7, num8) // num7: 50, num8: 60 (int로 선언)
 }
---
+++ GO/타입.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func 타입() {
+	// 주로 사용하는 타입
+
+	var isBool bool = false
+	fmt.Printf("isBool: %t\n", isBool) // isBool: true (bool로 선언)
+
+	var num1 int = 10
+	fmt.Printf("num1: %d\n", num1) // numInt: 10 (int로 선언)
+	// int8, int16, int32, int64
+	// int8 : -128 ~ 127
+	// int16 : -32768 ~ 32767
+	// int32 : -2147483648 ~ 2147483647
+	// int64 : -9223372036854775808 ~ 9223372036854775807
+
+	var num2 uint = 20
+	fmt.Printf("num2: %d\n", num2) // num2: 20 (uint로 선언)
+	// uint8, uint16, uint32, uint64
+	// uint8 : 0 ~ 255
+	// uint16 : 0 ~ 65535
+	// uint32 : 0 ~ 4294967295
+	// uint64 : 0 ~ 18446744073709551615
+
+	var num3 float32 = 30.1
+	fmt.Printf("num3: %f\n", num3) // num3: 30.100000 (float32로 선언)
+	// float32, float64
+	// float32 : 32비트 부동소수점
+	// float64 : 64비트 부동소수점
+
+	var num4 byte = 40
+	fmt.Printf("num4: %d\n", num4) // num4: 40 (byte로 선언)
+	// 1 byte = 8 bit
+	// 0000 0000 ~ 1111 1111 (0 ~ 255)
+
+	var string1 string = "Hello, World!"
+	fmt.Printf("string1: %s\n", string1) // string1: Hello, World! (string으로 선언)
+	// 문자열은 ""로 감싸서 표현
+
+
+	// 형변환
+	var float1 float32 = 3.14
+	fmt.Printf("float1: %f\n", float1) // float1: 3.140000 (float32로 선언)
+
+	fmt.Println("int(float1):", int(float1)) // int(float1): 3 (int로 형변환)
+
+
+	// %T : 타입
+	fmt.Printf("isBool: %T\n", isBool) // isBool: bool
+	// 타입을 출력할 때 사용
+}
