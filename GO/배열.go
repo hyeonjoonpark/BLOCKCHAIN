1a651fae984761c01dd49fbdc8caf13618371dd8
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-03-30 21:25:35 +0900

    배열

---
+++ GO/배열.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func 배열() {
+	// 배열
+	var arr [3]int
+	arr = [3]int{1, 2, 3}
+
+	fmt.Println(arr)
+	fmt.Printf("%T\n", arr)
+
+	length := len(arr) // 배열 길이
+	println(length)
+
+	// 배열 순회
+	for i := 0; i < len(arr); i++ {
+		println(arr[i])
+	}
+
+	// 다중 배열
+	var arr2 [3][3]int
+	arr2 = [3][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	fmt.Println(arr2[1][1])
+}
