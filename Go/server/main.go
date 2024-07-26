f03986f08eff34c9a6b227f6b5ff7e428756b590
hyeonjoonpark <pjjoon1379@gmail.com>
2024-07-26 21:59:05 +0900

    ADD: print

--- Go/server/main.go
+++ Go/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("public")))
+	fmt.Println("Server is running on port 8081")
 	http.ListenAndServe(":8081", nil)
 }
